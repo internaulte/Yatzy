fbe680bb47efae32e6e2a4a406303611d9ac5022
Laurent Nault <laurent.nault@igrafx.com>
2023-02-07 23:19:47 +0100

    naive implements pairs and two pairs

--- src/main/java/Yatzy.java
+++ src/main/java/Yatzy.java
@@ -1,76 +1,62 @@
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Map;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-import static java.util.stream.Collectors.counting;
-
 public class Yatzy {
 
-    public static int chance(int d1, int d2, int d3, int d4, int d5) {
+    public static int chance(final int d1, final int d2, final int d3, final int d4, final int d5) {
         return d1 + d2 + d3 + d4 + d5;
     }
 
-    public static int yatzy(int d1, int d2, int d3, int d4, int d5) {
+    public static int yatzy(final int d1, final int d2, final int d3, final int d4, final int d5) {
         boolean isYatzy = IntStream.of(d1, d2, d3, d4).allMatch(value -> value == d5);
 
         return isYatzy ? 50 : 0;
     }
 
-    public static int ones(int d1, int d2, int d3, int d4, int d5) {
+    public static int ones(final int d1, final int d2, final int d3, final int d4, final int d5) {
         return sumTargetValues(IntStream.of(d1, d2, d3, d4, d5), 1);
     }
 
-    public static int twos(int d1, int d2, int d3, int d4, int d5) {
+    public static int twos(final int d1, final int d2, final int d3, final int d4, final int d5) {
         return sumTargetValues(IntStream.of(d1, d2, d3, d4, d5), 2);
     }
 
-    public static int threes(int d1, int d2, int d3, int d4, int d5) {
+    public static int threes(final int d1, final int d2, final int d3, final int d4, final int d5) {
         return sumTargetValues(IntStream.of(d1, d2, d3, d4, d5), 3);
     }
 
-    public static int fours(int d1, int d2, int d3, int d4, int d5) {
+    public static int fours(final int d1, final int d2, final int d3, final int d4, final int d5) {
         return sumTargetValues(IntStream.of(d1, d2, d3, d4, d5), 4);
     }
 
-    public static int fives(int d1, int d2, int d3, int d4, int d5) {
+    public static int fives(final int d1, final int d2, final int d3, final int d4, final int d5) {
         return sumTargetValues(IntStream.of(d1, d2, d3, d4, d5), 5);
     }
 
-    public static int sixes(int d1, int d2, int d3, int d4, int d5) {
+    public static int sixes(final int d1, final int d2, final int d3, final int d4, final int d5) {
         return sumTargetValues(IntStream.of(d1, d2, d3, d4, d5), 6);
     }
 
-    private static int sumTargetValues(@NotNull IntStream dices, int targetValue) {
+    private static int sumTargetValues(@NotNull final IntStream dices, final int targetValue) {
         return dices.reduce(0, (acc, value) -> value == targetValue ? acc + value : acc);
     }
 
-    public static int pair(int d1, int d2, int d3, int d4, int d5) {
-        Map<Integer, Long> a = IntStream.of(d1, d2, d3, d4, d5).boxed().collect(Collectors.groupingBy(diceValue -> diceValue, counting()));
-        int toto = a.entrySet().stream().filter(entry -> entry.getValue() >= 2).map(Map.Entry::getKey).max(Integer::compareTo).orElse(0);
+    public static int pair(final int d1, final int d2, final int d3, final int d4, final int d5) {
+        final Set<Integer> allItems = new HashSet<>();
+        final int maxPairValueOrZero = IntStream.of(d1, d2, d3, d4, d5).filter(n -> !allItems.add(n)).max().orElse(0);
 
-        return toto * 2;
+        return maxPairValueOrZero * 2;
     }
 
-    public static int two_pairs(int d1, int d2, int d3, int d4, int d5) {
-        int[] counts = new int[6];
-        counts[d1 - 1]++;
-        counts[d2 - 1]++;
-        counts[d3 - 1]++;
-        counts[d4 - 1]++;
-        counts[d5 - 1]++;
-        int n = 0;
-        int score = 0;
-        for (int i = 0; i < 6; i += 1)
-            if (counts[6 - i - 1] >= 2) {
-                n++;
-                score += (6 - i);
-            }
-        if (n == 2)
-            return score * 2;
-        else
-            return 0;
+    public static int two_pairs(final int d1, final int d2, final int d3, final int d4, final int d5) {
+        final Set<Integer> allItems = new HashSet<>();
+        final Set<Integer> allPairsValues = IntStream.of(d1, d2, d3, d4, d5).boxed().filter(n -> !allItems.add(n)).collect(Collectors.toSet());
+
+        return allPairsValues.size() == 2 ? allPairsValues.stream().reduce(0, Integer::sum) * 2 : 0;
     }
 
     public static int three_of_a_kind(int d1, int d2, int d3, int d4, int d5) {
